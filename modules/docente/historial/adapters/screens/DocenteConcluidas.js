6a4e5f73887c2747f9757d90efae6243b7bf1e6f
LuisKevL <20213tn118@utez.edu.mx>
2023-04-13 20:00:09 -0600

    DocConclu

--- modules/docente/historial/adapters/screens/DocenteConcluidas.js
+++ modules/docente/historial/adapters/screens/DocenteConcluidas.js
@@ -1,201 +1,40 @@
-import {
-    StyleSheet,
-    Text,
-    View,
-    Image,
-    StatusBar,
-    SafeAreaView,
-  } from "react-native";
-  import React from "react";
-  import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-  import { Card } from "react-native-elements";
-  import { CardDivider } from "@rneui/base/dist/Card/Card.Divider";
-  import { Button, Icon } from "@rneui/themed";
-  
-  const backImage = require("../../../../assets/backImage.png");
-  
-  export default function DocenteConcluidas() {
-    return (
-      <KeyboardAwareScrollView style={styles.container}>
-        <Image source={backImage} style={styles.backImage} />
-        <View style={styles.whiteSheet} />
-        <SafeAreaView style={styles.form}>
-          <Text style={styles.title}>Concluidas</Text>
-  
-          <Card containerStyle={{ borderRadius: 20, height: 140 }}>
-            <Card.Title>01 CC7 Monitor Dañado</Card.Title>
-            <Card.Title style={{ color: "#008f71" }}>Concluidas</Card.Title>
-  
-            <View
-              style={{ flexDirection: "row", justifyContent: "space-between" }}
-            >
-              <Button
-                buttonStyle={{
-                  borderRadius: 10,
-                  marginLeft: 10,
-                  marginRight: 10,
-                  marginBottom: 0,
-                  height: 38,
-                  width: 140,
-                  backgroundColor: "#01315e",
-                }}
-                title="Open Incident"
-                titleStyle={{ fontWeight: "700" }}
-                iconRight
-                icon={<Icon name="chat" color={"white"} />}
-              />
-              <Button
-                buttonStyle={{
-                  borderRadius: 10,
-                  marginLeft: 10,
-                  marginRight: 10,
-                  marginBottom: 0,
-                  height: 38,
-                  width: 140,
-                  backgroundColor: "#01315e",
-                }}
-                title="Open Chat"
-                titleStyle={{ fontWeight: "700" }}
-                iconRight
-                icon={<Icon name="chat" color={"white"} />}
-              />
-            </View>
-          </Card>
-  
-          <CardDivider />
-  
-          <Card containerStyle={{ marginTop: 1, borderRadius: 20, height: 140 }}>
-            <Card.Title>02 CC7 Monitor Dañado</Card.Title>
-            <Card.Title style={{ color: "#008f71" }}>Concluidas</Card.Title>
-  
-            <View
-              style={{ flexDirection: "row", justifyContent: "space-between" }}
-            >
-              <Button
-                buttonStyle={{
-                  borderRadius: 10,
-                  marginLeft: 10,
-                  marginRight: 10,
-                  marginBottom: 0,
-                  height: 38,
-                  width: 140,
-                  backgroundColor: "#01315e",
-                }}
-                title="Open Incident"
-                titleStyle={{ fontWeight: "700" }}
-                iconRight
-                icon={<Icon name="chat" color={"white"} />}
-              />
-              <Button
-                buttonStyle={{
-                  borderRadius: 10,
-                  marginLeft: 10,
-                  marginRight: 10,
-                  marginBottom: 0,
-                  height: 38,
-                  width: 140,
-                  backgroundColor: "#01315e",
-                }}
-                title="Open Chat"
-                titleStyle={{ fontWeight: "700" }}
-                iconRight
-                icon={<Icon name="chat" color={"white"} />}
-              />
-            </View>
-          </Card>
-  
-          <CardDivider />
-  
-          <Card containerStyle={{ marginTop: 1, borderRadius: 20, height: 140 }}>
-            <Card.Title>03 CC7 Monitor Dañado</Card.Title>
-            <Card.Title style={{ color: "#008f71" }}>Concluidas</Card.Title>
-  
-            <View
-              style={{ flexDirection: "row", justifyContent: "space-between" }}
-            >
-              <Button
-                buttonStyle={{
-                  borderRadius: 10,
-                  marginLeft: 10,
-                  marginRight: 10,
-                  marginBottom: 0,
-                  height: 38,
-                  width: 140,
-                  backgroundColor: "#01315e",
-                }}
-                title="Open Incident"
-                titleStyle={{ fontWeight: "700" }}
-                iconRight
-                icon={<Icon name="chat" color={"white"} />}
-              />
-              <Button
-                buttonStyle={{
-                  borderRadius: 10,
-                  marginLeft: 10,
-                  marginRight: 10,
-                  marginBottom: 0,
-                  height: 38,
-                  width: 140,
-                  backgroundColor: "#01315e",
-                }}
-                title="Open Chat"
-                titleStyle={{ fontWeight: "700" }}
-                iconRight
-                icon={<Icon name="chat" color={"white"} />}
-              />
-            </View>
-          </Card>
-        </SafeAreaView>
-        <StatusBar barStyle="light-content" />
-      </KeyboardAwareScrollView>
-    );
-  }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-    },
-    title: {
-      fontSize: 36,
-      fontWeight: "#fff",
-      color: "black",
-      alignSelf: "center",
-      marginTop: 70,
-      paddingBottom: 50,
-    },
-    input: {
-      backgroundColor: "#F6F7FB",
-      height: 58,
-      marginBottom: 10,
-      fontSize: 16,
-      borderRadius: 10,
-      padding: 12,
-  
-      marginTop: 20,
-    },
-    backImage: {
-      width: "100%",
-      height: 340,
-      position: "absolute",
-      top: 0,
-      resizeMode: "cover",
-    },
-    whiteSheet: {
-      width: "100%",
-      height: "75%",
-      position: "absolute",
-      bottom: 0,
-      backgroundColor: "#fff",
-      borderTopLeftRadius: 60,
-      borderTopRightRadius: 60,
-    },
-    logotype: {
-      width: "100%",
-      height: 150,
-      marginTop: 10,
-    },
-    detailIcon: {
-      marginRight: 10,
-    },
-  });
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios'
+
+const DocenteConcluidas = () => {
+const [docentes, setDocentes] = useState([]);
+
+const filterDocentes = (users) => {
+  return users.filter(user => user.roles.name.toLowerCase() === "docente");
+}
+
+useEffect(() => {
+  axios.get('http://192.168.0.200:8080/api-sirid/user/')
+    .then(response => {
+      if (Array.isArray(response.data)) { 
+        console.log(response.data)
+        const filteredDocentes = filterDocentes(response.data);
+        setDocentes(filteredDocentes);
+        console.log(filteredDocentes);
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}, []);
+
+return (
+  <View>
+  <Text>Docentes:</Text>
+  <Text>Aquí se muestran los docentes:</Text>
+  {docentes && docentes.map(docente => (
+    <View key={docente.id}>
+      <Text>{docente.name} {docente.primerApellido} {docente.segundoApellido}</Text>
+      <Text>{docente.correoElectronico}</Text>
+    </View>
+  ))}
+</View>
+);
+}
+export default DocenteConcluidas;
